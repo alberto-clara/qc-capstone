7e0c2490a3211478ffe9df3db6da9e945b88a8eb
Phu <phu.pham@wsu.edu>
2019-11-02 21:56:01 -0800

    Set-up Tailwind

--- front-end/react-docker-app/src/components/Home.jsx
+++ front-end/react-docker-app/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../tailerUse.css'
 import { Link } from 'react-router-dom';
 export const Home = props => (
     <div>
-        <h1>Hello, world Home!</h1>
-        <Link to="/signinpage">
-            SigninPlease</Link>
+        <h1 className="text text-red-500 text-center font-black" >Hello, world Home!</h1>
         
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ">
+            Button
+        </button>
     </div>
 );
--- front-end/react-docker-app/src/components/SignInPage.jsx
+++ front-end/react-docker-app/src/components/SignInPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
-export const SignInPage = props => (
-    <div>
-        <h1>SignInPage!</h1>
-     
-    </div>
-);
+import React, { Component } from "react";
+import { Col, Row, Form, FormGroup, FormControl, FormText } from 'react-bootstrap';
+import '../tailerUse.css';
+export class SignInPage extends Component {
+
+    render() { 
+        return (
+            <div>
+                <h1>SignInPage!</h1> 
+        </div>
+        )
+
+    }
+}
