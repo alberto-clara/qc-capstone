0176632aad10c2360a0a408966ab02793d0f843d
KyleVoos <43186089+KyleVoos@users.noreply.github.com>
2019-11-03 16:38:41 -0800

    API Gateway Init
    generic api gateway, does not connect to enviornment
    
    Co-Authored-By: skylarcruz <skylarcruz@users.noreply.github.com>

---
+++ back-end/Api/CatalogApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogApi.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class CurrenciesController : ControllerBase
+    {
+        // GET api/currencies
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "usd", "inr", "jpy" };
+        }
+    }
+}
