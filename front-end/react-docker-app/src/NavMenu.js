fb6019812d74bcd9859d3d04a233beb445fb3a33
alberto.clara <alberto.clara@wsu.edu>
2019-11-08 09:51:07 -0800

    fixed button for sign-up and sign-in

--- front-end/react-docker-app/src/NavMenu.js
+++ front-end/react-docker-app/src/NavMenu.js
@@ -4,7 +4,7 @@ import NavTitle from './NavTitle';
 
 export default props => ( 
     <nav class="lg:w-full justify-end flex items-center flex-wrap bg-orange-600 lg:px-20 pb-5 pr-5 pl-5">
-        <div id="main-nav" class="flex-row w-full block flex-grow lg:flex lg:flex-grow lg:items-center w-auto">
+        <div id="main-nav" class="flex-row w-full block flex-grow lg:flex lg:flex-grow w-auto">
             <div class="text-sm lg:flex-grow">
                 <a href="/" class="lg:text-lg block mt-4 lg:inline-block text-orange-100 hover:text-white hover:font-bold mr-6">
                     Home
@@ -19,15 +19,19 @@ export default props => (
                     Contact
                 </a>
             </div>
-            <div class="object-center .w-3/4 lg:pt-0 sm:pt-12">
-                <a href="SignInPage" class="lg:text-lg inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-500 hover:bg-white">
-                    Sign-In
-                </a>
+            <div class="pt-2">
+                <Link to={'/signinpage'}>
+                    <button class="lg:w-auto sm:w-24 py-2 px-4 inline-block leading-none border rounded text-white text-sm lg:text-lg hover:border-transparent hover:text-orange-500 hover:bg-white">
+                            Sign-In
+                    </button>
+                </Link>
             </div>
-            <div class="object-center .w-3/4 lg:pt-0 sm:pt-2">
-                <a href="SignUpPage" class="lg:text-lg inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-500 hover:bg-white">
-                    Create Account
-                </a>
+            <div class="pt-2">
+                <Link to={'/signuppage'}>
+                    <button class="lg:w-auto sm:w-24 py-2 px-4 inline-block leading-none border rounded text-white text-sm lg:text-lg hover:border-transparent hover:text-orange-500 hover:bg-white">
+                            Sign-Up
+                    </button>
+                </Link>
             </div>
         </div>
     </nav>
@@ -35,23 +39,11 @@ export default props => (
 
     // Navbar Toggle
 document.addEventListener('DOMContentLoaded', function () {
-
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        // Get the "main-nav" element
-        var $target = document.getElementById('main-nav')
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0); // Get all "navbar-burger" elements
+    if ($navbarBurgers.length > 0) { // Check if there are any navbar burgers
+        var $target = document.getElementById('main-nav') // Get the "main-nav" element
         $target.classList.toggle('hidden');
-
-      // Add a click event on each of them
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {  
-          // Toggle the class on "main-nav"
-          $target.classList.toggle('hidden');
-  
-        });
-      });
+        // Add a click event on each of them // Toggle the class on "main-nav"
+        $navbarBurgers.forEach(function ($el) {$el.addEventListener('click', function(){$target.classList.toggle('hidden');});});
     }
   });
