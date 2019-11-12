a0e1601bbb76359fe908c91e587559ed50f049ff
Phu <phu.pham@wsu.edu>
2019-11-11 17:22:45 -0800

    Update Home.jsx

--- front-end/react-docker-app/src/components/Home.jsx
+++ front-end/react-docker-app/src/components/Home.jsx
@@ -38,14 +38,3 @@ export function Home(props) {
         </div>
     );
 }
-
-=======
-import React from 'react';
-import '../tailerUse.css';
-import '../NavMenu.js';
-export const Home = props => (
-    <div>
-        <h1 className="text text-red-500 text-center font-black" >Hello, Home Depot!</h1>
-    </div>
-);
->>>>>>> NavBar
