6c32a07c441309e92746761eb323bd1ae00ab3b0
KyleVoos <43186089+KyleVoos@users.noreply.github.com>
2019-11-11 16:27:56 -0800

    Firebase Admin SDK
    Co-Authored-By: Muhannad Qaisi <muhannadqaisi@users.noreply.github.com>
    Co-Authored-By: skylarcruz <skylarcruz@users.noreply.github.com>

--- back-end/Api/ApiGateway/Startup.cs
+++ back-end/Api/ApiGateway/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Ocelot.Middleware;
 using Ocelot.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ApiGateway
 {
@@ -27,6 +29,21 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "placeholder";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = "palceholder",
+                        ValidateAudience = true,
+                        ValidAudience = "placeholder",
+                        ValidateLifetime = true
+                    };
+                }
+                );
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddOcelot(Configuration);
         }
