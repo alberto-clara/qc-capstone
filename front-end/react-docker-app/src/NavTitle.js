bab9745e02eeba650bc1704cfe55b02b3ef9dda2
alberto.clara <alberto.clara@wsu.edu>
2019-11-12 16:25:55 -0800

    added search bar

--- front-end/react-docker-app/src/NavTitle.js
+++ front-end/react-docker-app/src/NavTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default props => ( 
-    <nav class="flex items-center flex-wrap bg-orange-600 lg:px-20 py-2 pl-5 pr-5">
+    <nav class="flex flex-wrap bg-orange-600 lg:px-20 py-2 pl-5 pr-5">
         <div class="lg:hidden bg-orange-600 pr-4">
             <button class="navbar-burger px-2 py-2 border rounded text-white hover:text-white hover:border-white">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
@@ -13,7 +13,7 @@ export default props => (
                 <span class="font-bold lg:text-3xl sm:text-xl">Quote Center</span>
             </Link>
         </div>
-        <div class="object right">
+        <div>
             <button class="px-2 py-2 border rounded text-orange-200 border-orange-600 hover:text-white hover:border-white">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><title>Cart</title><path class="heroicon-ui" fill="white" d="M17 16a3 3 0 1 1-2.83 2H9.83a3 3 0 1 1-5.62-.1A3 3 0 0 1 5 12V4H3a1 1 0 1 1 0-2h3a1 1 0 0 1 1 1v1h14a1 1 0 0 1 .9 1.45l-4 8a1 1 0 0 1-.9.55H5a1 1 0 0 0 0 2h12zM7 12h9.38l3-6H7v6zm0 8a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm10 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/></svg>
             </button>
--- front-end/react-docker-app/src/components/Home.jsx
+++ front-end/react-docker-app/src/components/Home.jsx
@@ -3,6 +3,12 @@ import '../tailerUse.css';
 import '../NavMenu.js';
 export const Home = props => (
     <div>
+        <div class="mt-4 justify-center flex text-gray-600 lg:px-20 pl-5 pr-5">
+            <input class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-8 text-sm" type="search" name="search" placeholder="Search"></input>
+            <button type="submit" class="px-4 py-2 border-2 rounded text-gray-300 border-gray-300 bg-white hover:text-white hover:border-orange-400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" fill="gray" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+            </button>
+        </div>
         <h1 className="text text-red-500 text-center font-black" >Hello, Home Depot!</h1>
     </div>
 );
