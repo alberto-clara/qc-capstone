4b419b57b09c7eb761bcb24cc012e5cc8231ca85
fabi <fabiane.yeung@wsu.edu>
2019-11-27 00:55:31 -0800

    Auto stash before rebase of "origin/fetch"

--- front-end/react-docker-app/src/components/Home.jsx
+++ front-end/react-docker-app/src/components/Home.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import '../css/mainTailwind.css';
 
 import { Slider } from './AdSlide';
+
+export const searchbar = (
+
+    <div className="  mt-4 justify-center flex text-gray-600 lg:px-20 pl-5 pr-5">
+        <input className=" rounded w-full lg:w-1/2 border-2 border-gray-300 bg-white h-10 px-5 pr-8 text-sm" type="search" name="search" placeholder="Search"></input>
+        <button type="submit" className="px-4 py-2 border-2 rounded text-gray-300 border-gray-300 bg-white hover:text-white hover:border-orange-400">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="20"><path className="heroicon-ui " fill="gray" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" /></svg>
+        </button>
+    </div>);
+
 export const Home =(props)=> {
+
     const fetching=(response) => {
 
         fetch("http://localhost:7000/catalog-api/products/page?pageSize=3&pageIndex=10", { mode: 'cors' })
@@ -47,6 +58,7 @@ export const Home =(props)=> {
     return (
         <div  >
             {homediv}
+            {searchbar}
 
             {/*<Slider /> */} 
            
--- front-end/react-docker-app/src/components/Product.jsx
+++ front-end/react-docker-app/src/components/Product.jsx
@@ -1,25 +1,35 @@
 import React, { useState, useContext } from 'react';
 import '../css/mainTailwind.css';
+import { searchbar } from '../components/Home'
 
 export function Product(props) {
-   
-    const search = (
 
-            <div className="  mt-4 justify-center flex text-gray-600 lg:px-20 pl-5 pr-5">
-                <input className=" rounded w-full lg:w-1/2 border-2 border-gray-300 bg-white h-10 px-5 pr-8 text-sm" type="search" name="search" placeholder="Search"></input>
-                <button type="submit" className="px-4 py-2 border-2 rounded text-gray-300 border-gray-300 bg-white hover:text-white hover:border-orange-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="20"><path className="heroicon-ui " fill="gray" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" /></svg>
-                </button>
-            </div>);
-    
     const side_pic = (
-        <div className=" titlePage py-2 lg:text-3xl"> Products</div>
+        // <div className="h-64 ti:h-72 md:h-76 lg:justify-center lg:px-50 pr-5 pl-5">
+        //     <div className=" titlePage py-2 lg:text-3xl"> Product View </div>
+        //     <div className="bg-gray-200 h-78 w-32 -my-px-40">
+        //     <div className="py-4 flex justify-center bg-blue-200 md:h-70 w-100"></div>
+        //     </div>
+
+        // </div>
+        <div class="relative bg-gray-400 h-64 ti:h-72 md:h-76 lg:justify-center lg:px-50 pr-5 pl-5">
+            <div className=" titlePage py-2 lg:text-3xl"> Product View </div>
+            <div class="static bg-gray-200 h-78 w-32 -my-px-40">
+                Static parent
+
+                <div class="bg-gray-400 inline-block">
+                    Static sibling
+                </div>
+            </div>
+        </div>
+
+
     );
 
     return (
         <div>
-            {search}
+            {searchbar}
             {side_pic}
         </div>
-        )
+    )
 }
