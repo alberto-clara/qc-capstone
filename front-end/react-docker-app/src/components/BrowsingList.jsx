538f9fc06403ab9a8aacef40a6348cd2f1ff95a7
alberto.clara <alberto.clara@wsu.edu>
2019-12-03 20:00:56 -0800

    Updated browsing Page

--- front-end/react-docker-app/src/components/BrowsingList.jsx
+++ front-end/react-docker-app/src/components/BrowsingList.jsx
@@ -85,12 +85,12 @@ export const BrowsingList = (props) => {
                 `<a target="_blank"><img src="https://images.homedepot-static.com/productImages/8a89c543-1c72-4e6e-972f-0e5babb15a10/svn/husky-claw-hammers-n-s20shd-hn-64_400_compressed.jpg" width="150" height="112" alt="Hammer"/></a>` +
                 `</div>` +
                 `<div class="inline-block flex-1 px-4 py-1 lg:px-8 lg:py-1 ">` +
-                `<a class="text-sm lg:text-xl hover:bg-gray-200" href="product/` + items[i].id + `">` + items[i].product_name + `</a>` +
+                `<a class="text-black text-sm lg:text-xl hover:bg-gray-200" href="product/` + items[i].id + `">` + items[i].product_name + `</a>` +
                 `<div class="text-xs lg:text-lg pb-4 lg:hidden unit_retail">` + 'Cost: $' + items[i].unit_retail + `</div>` +
                 // `<div class="text-lg flex-wrap product_id">` + items[i].id + `</div>`+
                 `<div class="text-xs lg:text-md flex-wrap">` + `Delivers Today.` + `</div>` +
                 `<div class="flex content-end">` +
-                `<div class="text-xs lg:text-md flex pt-2 lg:pt-12">` + `Add to Favorites` + `</div>` +
+                `<div class="text-xs lg:text-md flex pt-12 lg:pt-24 hover:text-blue-600">` + `Add to Favorites` + `</div>` +
                 `</div>` +
                 `</div>` +
                 `<div class="hidden lg:flex px-4 py-2 lg:px-8 lg:py-4">` +
@@ -115,36 +115,50 @@ export const BrowsingList = (props) => {
 
     return (<>
         <div className="mt-4 justify-center w-full h-auto md:h-auto">
-            <div className=" titlePage py-2 lg:text-3xl"> Browsing View </div>
+            <div className=" titlePage pt-2 pb-4 lg:text-3xl"> Browsing View </div>
         </div>
-        <div className="flex max-w-full sm:px-8 lg:px-20">
-            <div className="hidden lg:block flex-1 w-1/5 rounded-lg border /*bg-gray-100*/ mr-2 h-auto">
-                <div className="flex justify-center text-xl pt-4">
-                    Settings
+        <div className="lg:flex max-w-full sm:px-8 lg:px-20">
+            <div className="block lg:w-1/5 rounded-lg border /*bg-gray-100*/ lg:mr-2 h-auto">
+                <div className="flex justify-center">
+                    <button className="filterbutton flex lg:hidden lg:text-black font-bold text-lg py-4">
+                        Filters
+                    </button>
+                    <div className="filterbutton hidden lg:flex text-blue-600 lg:text-black font-bold text-2xl pt-4 pl-4">
+                        Filters
+                    </div>
                 </div>
-                <div className="flex-wrap text-lg px-8 py-4 block-inline">
-                    <div className="flex flex-wrap text-lg block">
+                <div id="filter" className="block flex-wrap text-lg px-8 py-4">
+                    <div className="flex flex-wrap pt-2 text-lg justify-center lg:justify-start font-bold">
                         Sort by:
                     </div>
-                    <div className="flex-wrap text-md px-4">
-                        <div className="flex">
-                            <div className="w-1/3">Price</div>
-                            <button onClick={() => { setFetchSort('ascending') }} className="w-1/3 text-red-500 font-bold"> Highest&#8593; </button>
-                            <button onClick={() => { setFetchSort('descending') }} className="w-1/3 text-green-500 font-bold">Lowest&#8595; </button>
-                            <button onClick={() => { setFetchSort('') }} className="w-1/3 font-bold">x</button>
+                    <div className="px-2">
+                        <div className="flex justify-center lg:justify-start lg:block p-2">
+                            <div className="">Price:</div>
+                            <button onClick={() => { setFetchSort('ascending') }} className="hover:text-blue-600 px-2"> Highest </button>
+                            <button onClick={() => { setFetchSort('descending') }} className="hover:text-blue-600 px-2">Lowest </button>
+                            {/* <button onClick={() => { setFetchSort('') }} className="w-1/3 font-bold">x</button> */}
                         </div>
-                        <div>
-                            Ratings
-                        </div>
-                        <div>
-                            Brand
-                        </div>
-                        <div>
-                            Type
-                        </div>
+                        {/* <div className="flex justify-center lg:justify-start p-2">
+                        <div className="">Rating:</div>
+                            <button onClick={() => { setFetchSort('ascending') }} className="hover:text-blue-600 px-2"> Highest </button>
+                            <button onClick={() => { setFetchSort('descending') }} className="hover:text-blue-600 px-2">Lowest </button>                        </div>
+                        <div className="flex justify-center lg:justify-start p-2">
+                        <div className="">Brand:</div>
+                            <button onClick={() => { setFetchSort('ascending') }} className="hover:text-blue-600 px-2"> Highest </button>
+                            <button onClick={() => { setFetchSort('descending') }} className="hover:text-blue-600 px-2">Lowest </button>
+                        </div> */}
                     </div>
+                    
+                    <div className="block py-4">
+                        <div className="text-lg font-bold">
+                            Items per Page:
+                        </div>
+                        <Dropdown className="justify-end" options={options} value={pageNumber.toString()} onChange={e => changeSize(e)} />
+                    </div>
+
                 </div>
             </div>
+
             <div className="w-full lg:w-4/5">
                 <div>{load ? loopfetching(pageNumber, pageLocation)  : null}  </div>
                 <div id="container" /*className="bg-orange-100"*/> </div>
@@ -152,12 +166,12 @@ export const BrowsingList = (props) => {
         </div>
         
         <div className="bg-blue-100 flex justify-center px-8 lg:px-20">
-            <div className="flex-1 justify-center text-center ">
+            <div className="hidden lg:flex justify-center text-center ">
                 <Pagination onPageChange={(e, data) => changePage(data.activePage)}  boundaryRange={1}
                     totalPages={countPage}
                     activePage={activePage} />
             </div>
-            {/* <div className="flex-1 cursor-pointer lg:hidden text-center">
+            <div className="flex-1 cursor-pointer lg:hidden text-center">
                 <Pagination onPageChange={(e, data) => changePage(data.activePage)} defaultActivePage={1}
                     ellipsisItem={null}
                     boundaryRange={0}
@@ -165,14 +179,18 @@ export const BrowsingList = (props) => {
                     lastItem={null}
                     siblingRange={1}
                     totalPages={countPage} />
-            </div> */}
-            <div className="hidden lg:flex justify-end bg-gray-100">
-                <div className="largeBold pr-8">
-                    Size:
-                </div>
-                <Dropdown className=" w-24 " options={options} value={pageNumber.toString()} onChange={e => changeSize(e)} />
             </div>
         </div>
 
     </>);
 }
+// Navbar Toggle
+document.addEventListener('DOMContentLoaded', function () {
+    var $filterbutton = Array.prototype.slice.call(document.querySelectorAll('.filterbutton'), 0); // Get all "navbar-burger" elements
+    if ($filterbutton.length > 0) { // Check if there are any navbar burgers
+        var $target = document.getElementById('filter') // Get the "filter" element
+        $target.classList.toggle('visible');
+        // Add a click event on each of them // Toggle the className on "filter"
+        $filterbutton.forEach(function ($el) {$el.addEventListener('click', function(){$target.classList.toggle('hidden');});});
+    }
+});
