f2d915c60c8af14f016d51b6b308acb24227a42a
KyleVoos <43186089+KyleVoos@users.noreply.github.com>
2020-05-07 12:04:44 -0700

    CheckoutController and EmailSender comments

--- back-end/Api/CheckoutApi/Controllers/CheckoutController.cs
+++ back-end/Api/CheckoutApi/Controllers/CheckoutController.cs
@@ -24,10 +24,12 @@ namespace CheckoutApi.Controllers
     [ApiController]
     public class CheckoutController : ControllerBase
     {       
-        
+        // Couchbase buckets
         private IBucket _checkoutBucket;
         private IBucket _userInfoBucket;
+        // environment, used to locate files needed for sending email
         private IHostingEnvironment _env;
+        // interface used to send the email
         private readonly IEmailSender _emailSender;
         private Configuration configuration { get; set; }
 
@@ -65,51 +67,68 @@ namespace CheckoutApi.Controllers
         }
 
         /*
+         * Route to add a completed order to the users Checkout document.
          * PUT (CheckoutApi) http://localhost:7004/api/checkout/add
          * PUT (through APIGateway) http://localhost:7000/checkout-api/checkout/add
          */
         [HttpPut, Route("add")]
         public async Task<IActionResult> add([FromHeader] string authorization)
-        {           
+        {  
+            // get the user_id from the decrypted Firebase token, stored in the HttpContext
             var currentUser = HttpContext.User;
             if (!currentUser.HasClaim(c => c.Type == "user_id"))
             {
                 return BadRequest();
             }
             var ID = currentUser.Claims.FirstOrDefault(c => c.Type == "user_id").Value;
+            // split the authorization header string to remove 'Bearer' and isolate the JWT token
             string[] auth = authorization.Split(' ');
 
+            // build a new HTTP request to the BasketAPI to get the users Basket document
             HttpClient client = new HttpClient();
+            // configuration.APIGATEWAYURL is an environment variable - found in appsettings.{env}.json
             client.BaseAddress = new Uri($"{configuration.APIGATEWAYURL}/basket-api/basket/find");
+            // add the JWT token to the auth header in the HTTP request, doesn't work to just add the full one stored in authorization
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", auth[1]);
+            // set the Content-Type header as application/json
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            // send the HTTP request and await the response
             HttpResponseMessage httpResponse = await client.GetAsync(client.BaseAddress);
 
+            // make sure the response from the interprocess communication was successful
             if (!httpResponse.IsSuccessStatusCode)
                 return BadRequest(httpResponse.StatusCode);
 
+            // get the users Checkout document
             var userDoc = await _checkoutBucket.GetAsync<Checkout>(ID);
 
+            // get the users UserInfo document
             var userInfoDoc = await _userInfoBucket.GetAsync<UserInfo>(ID);
 
+            // make sure the UserInfo document was successfully retreived
             if (!userInfoDoc.Success)
             {
                 if (userInfoDoc.Status != ResponseStatus.KeyNotFound)
                     return NotFound(userInfoDoc.Message);
             }
 
+            // read the HTTP response from the BasketAPI as an Order object, this is the users current order
             Order order = await httpResponse.Content.ReadAsAsync<Order>();
 
+            // update the information about the order, formated date in local time, generate a new GUID, add the shipping info
             order.date = DateTime.Now.ToLocalTime().ToString("MM/dd/yyyy HH:mm tt");
             order.OrderId = Guid.NewGuid();
-            order.Shipping = userInfoDoc.Value;
+            order.Shipping = userInfoDoc.Value;            
             Checkout checkout = null;
+            // check to see if a Checkout document was successfully retreived for the user
             if (!userDoc.Success)
             {
+                // make a new Checkout document for the user
                 checkout = new Checkout();
                 checkout.Orders = new List<Order>();
+                // add the Order that was read from the HTTP response earlier to the Order array of the new document
                 checkout.Orders.Add(order);
-
+                // insert the new Checkout document for the user into the database
                 var result = await _checkoutBucket.UpsertAsync(ID, checkout);
 
                 if (!result.Success)
@@ -117,6 +136,7 @@ namespace CheckoutApi.Controllers
             }
             else
             {
+                // if a Checkout doc already existed for the user add the new Order to the start of the array
                 checkout = userDoc.Value;
                 checkout.Orders = checkout.Orders.Prepend(order).ToList();
 
@@ -126,15 +146,21 @@ namespace CheckoutApi.Controllers
                     return NotFound("!res.Success");
             }
 
+            // make another HTTP request to send to the BasketAPI to delete the items from the user Basket doc
             client = new HttpClient();
             client.BaseAddress = new Uri($"{configuration.APIGATEWAYURL}/basket-api/basket/delete");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", auth[1]);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             httpResponse = await client.DeleteAsync(client.BaseAddress);
 
+            // make sure the reponse was successful
             if (!httpResponse.IsSuccessStatusCode)
                 return BadRequest(httpResponse.StatusCode);
 
+            /* only send an email if a UserInfo doc exists
+             * NOTE: This should not need to be done but the frontend never required that
+             * a user enter shipping information in before someone is purchasing their order
+             */
             if (userInfoDoc.Success)
             {
                 await SendEmail(ID, checkout.Orders[0]);
@@ -154,6 +180,7 @@ namespace CheckoutApi.Controllers
         [HttpPost, Route("addUserInfo")]
         public async Task<IActionResult> AddUserInfo([FromBody] UserInfo newUserInfo)
         {
+            // make sure the model bound correctly
             if (ModelState.IsValid)
             {
                 var currentUser = HttpContext.User;
@@ -163,8 +190,11 @@ namespace CheckoutApi.Controllers
 
                 var ID = currentUser.Claims.FirstOrDefault(c => c.Type == "user_id").Value;
 
+                // set the Uid that can be retreived from the Firebase token if its null
                 if (newUserInfo.Uid == null)
                     newUserInfo.Uid = ID;
+
+                // add the UserInfo document to the UserInfo bucket
                 var result = await _userInfoBucket.UpsertAsync(ID, newUserInfo);
 
                 if (!result.Success)
@@ -177,7 +207,6 @@ namespace CheckoutApi.Controllers
         }
 
         /* 
-         * Should be finished and working correctly
          * Route to get user info and send it to the frontend
          * GET (CheckoutApi) http://localhost:7004/api/checkout/getUserInfo
          * GET (through APIGateway) http://localhost:7000/checkout-api/checkout/getUserInfo
@@ -199,28 +228,36 @@ namespace CheckoutApi.Controllers
             return Ok(result.Value);
         }
 
+        // function called from HTTPPUT add to send an email after a purchased order
         private async Task SendEmail(string ID, Order order)
         {
+            // make sure the model bound correctly
             if (ModelState.IsValid)
             {
+                // get the html template document used to build the email
                 var fp = "Template"
                     + Path.DirectorySeparatorChar.ToString()
                     + "ConfirmOrder.html";
 
+                // subject line of the email
                 var subject = "qc-capstone Order Confirmation";
 
+                // a new body for the email being sent
                 var builder = new BodyBuilder();
 
-
+                // read the email template - located at ./Template/ConfrimOrder.html
                 using (StreamReader reader = System.IO.File.OpenText(fp))
                 {
                     builder.HtmlBody = reader.ReadToEnd();
                 }
 
                 string orders = null;
+                // build the name that will be formated into the email
                 string name = order.Shipping.Full_name.First_name + " " + order.Shipping.Full_name.Last_name;
+                // add the images to the email (never seemed to work correctly but everything I found shows this is how it was done)
                 var image = builder.LinkedResources.Add("Template/Email_Templatehtml_files/right.gif");
                 image.ContentId = MimeUtils.GenerateMessageId();
+                // Add a section of html code for each of the individual offering that were purchased by the user in this order
                 foreach (Offerings offering in order.Offerings)
                 {
                     orders += $@"<tr>
@@ -245,15 +282,14 @@ namespace CheckoutApi.Controllers
                                   </tr> ";
                 }
 
-                Console.WriteLine(orders);
+                // format all the variables and html body into a string that will be our email messages body
                 string messageBody = string.Format(builder.HtmlBody,
                     name,
                     order.OrderId,
                     orders
                     );
 
-                Console.WriteLine(messageBody);
-
+                // send the email: uses the interface located in CheckoutApi/Services
                 await _emailSender.SendEmailAsync(order.Shipping.Email, subject, messageBody, name);
             }
         }
--- back-end/Api/CheckoutApi/Services/EmailSender.cs
+++ back-end/Api/CheckoutApi/Services/EmailSender.cs
@@ -19,6 +19,7 @@ namespace CheckoutApi.Services
                 string Subject = subject;
                 string Body = message;
 
+                // create a new MimeMessage and add the sender and receiver information
                 var mimeMessage = new MimeMessage();
                 mimeMessage.From.Add(new MailboxAddress(
                     FromAddressTitle,
@@ -28,19 +29,28 @@ namespace CheckoutApi.Services
                     ToAddressTitle,
                     ToAddress
                     ));
+                // add the subject and the html body that was built in the CheckoutController
                 mimeMessage.Subject = Subject;
                 mimeMessage.Body = new TextPart(TextFormat.Html)
                 {
                     Text = Body
                 };
 
+                // send the email
                 using (var client = new SmtpClient())
                 {
+                    // we are using a gmail account to send the email so connect to Google's SMTP servers
+                    // SMTP server located at URL: smtp.gmail.com | PORT: 587
                     client.Connect("smtp.gmail.com", 587);
+                    // Remove OAUTH2 authentication
                     client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    // we want to use TLS not SSL
                     client.SslProtocols |= System.Security.Authentication.SslProtocols.Tls;
+                    // credentials to connect the email account
                     client.Authenticate("qc.capstone@gmail.com", "kylevoos");
+                    // send the email
                     await client.SendAsync(mimeMessage);
+                    // disconnect from the SMTP server
                     await client.DisconnectAsync(true);
                 }
             }
